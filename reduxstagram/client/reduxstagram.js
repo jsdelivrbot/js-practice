09c215691486091d9fe63978af5485295787ec45
WispAmulet <ylyzainali@gmail.com>
2017-05-23 17:44:49 +0800

    add something

---
+++ reduxstagram/client/reduxstagram.js
@@ -0,0 +1,12 @@
+// let's go!
+import React from 'react';
+
+import { render } from 'react-dom';
+
+// Import css
+import css from './styles/style.styl';
+
+render(
+  <p>Hi</p>,
+  document.getElementById('root')
+);
