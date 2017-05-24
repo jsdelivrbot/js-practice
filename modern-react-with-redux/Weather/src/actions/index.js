d9cf0be532986ce2d442a7e6b6ae199aaadb87d3
WispAmulet <ylyzainali@gmail.com>
2017-05-25 01:31:27 +0800

    update Weather

---
+++ modern-react-with-redux/Weather/src/actions/index.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const API_KEY = 'ea49dee30ad80b5f52d37bd4a3d4be51';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  console.log('Request: ', request);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
