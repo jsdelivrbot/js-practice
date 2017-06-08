35588174df7f8a66f2880bd2f47ebee29b137b48
WispAmulet <ylyzainali@gmail.com>
2017-06-08 08:58:28 +0800

    add 'load streetview'

--- udacity/minicourse-ajax-project/js/script.js
+++ udacity/minicourse-ajax-project/js/script.js
@@ -12,6 +12,21 @@ function loadData() {
     $nytElem.text("");
 
     // load streetview
+    const URL = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=';
+
+    // var street =  $('#form-container').children('input')[0];
+    // var city =  $('#form-container').children('input')[1];
+    var valOfStreet = $('#street').val();
+    var valOfCity = $('#city').val();
+    if (!valOfStreet || !valOfCity) {
+        $('h2').text('Hey, You need to type something!');
+    } else {
+        const url = `${URL}${valOfStreet}, ${valOfCity}`;
+        // console.log(valOfStreet, valOfCity);
+        $('body').append('<img />');
+        $('img').addClass('bgimg').attr('src', url);
+        $('h2').text(`So, you want to live at ${valOfStreet}, ${valOfCity}?`);
+    }
 
     // YOUR CODE GOES HERE!
 
