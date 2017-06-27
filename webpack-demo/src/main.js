af62b75a3754657bcc843ac7a1e67e4602f7b2ae
WispAmulet <ylyzainali@gmail.com>
2017-06-27 16:41:44 +0800

    add webpack-demo

---
+++ webpack-demo/src/main.js
@@ -0,0 +1,5 @@
+import { map } from 'lodash';
+
+const result = map([1, 2, 3, 4, 5, 6], n => n * n);
+
+console.log(result);
---
+++ webpack-demo/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          plugins: ['transform-runtime'],
+          presets: ['es2015']
+        }
+      }
+    ]
+  }
+};
